0a447ab41a1f29a53d1ce57ec68d33ff892298cb
Igor Perepilitsyn <perepilitsyn@cls.global>
2022-06-17 16:40:32 +0300

    refactor luhn, remove unused function

--- internal/utils/luhn.go
+++ internal/utils/luhn.go
@@ -1,14 +1,5 @@
 package utils
 
-func CalculateLuhn(number int64) int64 {
-	checkNumber := checksum(number)
-
-	if checkNumber == 0 {
-		return 0
-	}
-	return 10 - checkNumber
-}
-
 func IsValidLuhn(number int64) bool {
 	return (number%10+checksum(number/10))%10 == 0
 }
@@ -17,16 +8,16 @@ func checksum(number int64) int64 {
 	var luhn int64
 
 	for i := 0; number > 0; i++ {
-		cur := number % 10
+		digit := number % 10
 
-		if i%2 == 0 { // even
-			cur = cur * 2
-			if cur > 9 {
-				cur = cur%10 + cur/10
+		if i%2 == 0 {
+			digit = digit * 2
+			if digit > 9 {
+				digit = digit%10 + digit/10
 			}
 		}
 
-		luhn += cur
+		luhn += digit
 		number = number / 10
 	}
 	return luhn % 10
